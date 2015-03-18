0f502457dbe6e20826a3ccce20105968b276fca5
xs <197585312@qq.com>
2015-03-18 17:02:58 +0800

    0.2

--- lib/YFC_LIB/src/com/yfc_lib/util/Constants.java
+++ lib/YFC_LIB/src/com/yfc_lib/util/Constants.java
@@ -3,7 +3,9 @@ package com.yfc_lib.util;
 public class Constants {
 	public static long EXIT_TIME = 0;
 	public static final long EXIT_MAX_TIME = 2000;
-
 	public static final String CHAR_SET = "UTF-8";
 
+	public class KEY {
+		public static final String INTENT_DATA_KEY = "intent_data";
+	}
 }
